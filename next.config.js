90cffe2aa05a74072531b54c2e74b7a589965394
Nick <banterstudios@gmail.com>
2020-05-14 19:59:41 +0100

    Add raw-loader and glsl-loader to webpack

---
+++ next.config.js
@@ -0,0 +1,11 @@
+export default {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      use: ['raw-loader', 'glslify-loader'],
+    });
+
+    return config;
+  },
+};
